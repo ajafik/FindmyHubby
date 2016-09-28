eed5805c1c2b76037c70a3dd78fba206a473c9f5
Oseme Odigie <osemeodigie@Osemes-MacBook-Pro.local>
2016-09-28 16:28:41 +0100

    Added the hello.js module for client side auth.

---
+++ js/client_ids.js
@@ -0,0 +1,55 @@
+//
+// Client IDS
+// Defines the CLIENT_ID (AppID's) of the OAuth2 providers
+// relative to the domain host where this code is presented.
+
+var location_https = window.location.href.indexOf('https://') === 0;
+
+// Google Register --  https://console.developers.google.com
+var GOOGLE_CLIENT_ID = '656984324806-sr0q9vq78tlna4hvhlmcgp2bs2ut8uj8.apps.googleusercontent.com';
+
+
+// Register your domain with Facebook and add here
+var FACEBOOK_CLIENT_ID = {
+	'local.findmyhubby.com' : '190771111357216',
+}[window.location.hostname] || '190771111357216';
+
+// Register your domain with Twitter and add here
+var TWITTER_CLIENT_ID = {
+	'local.findmyhubby.com' : 'krGNvpEVVBE27jcemC6uA'
+}[window.location.hostname] || 'eQuyZuECKWPiv3D7E4qdg';
+
+
+// Instagram Register - http://instagram.com/developer/clients/manage/
+var INSTAGRAM_CLIENT_ID = {
+	'local.findmyhubby.com' : 'bfbbf362ac3148aeb1150e5b8256bbe9',
+	'adodson.com' : location_https ? 'e47d210f864c4b1ca94225ddab97205a' : '264d13a33ba845f396a152cc326e6f5d'
+}[window.location.hostname];
+
+
+
+// Put all the auth2 IDs in an object
+var CLIENT_IDS = {
+	google : GOOGLE_CLIENT_ID,
+	facebook : FACEBOOK_CLIENT_ID,
+	twitter : TWITTER_CLIENT_ID,
+	instagram : INSTAGRAM_CLIENT_ID
+};
+
+
+//
+// OAUTH PROXY
+//
+var OAUTH_PROXY_URL = {
+	'local.findmyhubby.com' : 'http://local.findmyhubby.com:8888/#result'
+}[window.location.hostname] || 'https://auth-server.herokuapp.com/proxy';
+
+
+
+//
+// The manin Redirect URI
+//
+var REDIRECT_URI = '/hello.js/redirect.html';
+if (typeof chrome === 'object' && typeof chrome.identity === 'object' && chrome.identity.launchWebAuthFlow) {
+	REDIRECT_URI = 'https://'+window.location.hostname+'.chromiumapp.org/redirect.html';
+}
---
+++ js/uiProfile.js
@@ -0,0 +1,50 @@
+// Profile UI
+// Displays the users profile details
+
+// Intiate App credentials
+hello.init({
+	google : CLIENT_IDS.google,
+	facebook : CLIENT_IDS.facebook,
+	twitter : CLIENT_IDS.twitter
+},{
+	scope : 'email',
+	redirect_uri: REDIRECT_URI
+});
+
+
+// Bind events to the buttons on the page
+$(document).on("click", "button.profile", function(e){
+	e.preventDefault();
+	hello(this.id).login();
+})
+
+
+// Listen to signin requests, get the data from the provider and post to the server, then unlock the hubby data.
+hello.on('auth.login', function(r) {
+	// Get Profile
+	hello( r.network ).api( '/me' ).then( function(data) {
+
+		// post the question answers here + the response from the auth inside the 'data' object.
+
+		// var label = document.getElementById(r.network);
+		// label.innerHTML = "<img src='"+ p.thumbnail + "' width=24/>Connected to "+ r.network+" as " + p.name;
+
+		// On chrome apps we're not able to get remote images
+		// This is a workaround
+		/* if (typeof(chrome) === 'object') {
+			img_xhr(label.getElementsByTagName('img')[0], p.thumbnail);
+		} */
+	});
+});
+
+
+// Utility for loading the thumbnail in chromeapp
+function img_xhr(img, url) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true);
+	xhr.responseType = 'blob';
+	xhr.onload = function(e) {
+		img.src = window.URL.createObjectURL(this.response);
+	};
+	xhr.send();
+}
